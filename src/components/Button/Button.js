144c903bf1e7c07b20838c680007ac2803f6cbc9
RobertPoghosyan <[robertpoghosian@gmail.com]>
2021-02-06 14:00:00 +0400

    Initial commit

---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Button.scss";
+
+const Button = () => {
+  return <button className="app-button">
+      
+  </button>;
+};
+
+export default Button;
---
+++ src/containers/Container/Container.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Container extends Component {
+  render() {
+    return <div>React BoilerPlate</div>;
+  }
+}
+
+export default Container;
